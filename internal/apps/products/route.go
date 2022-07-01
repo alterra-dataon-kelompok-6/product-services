71e5a82c35d9e053c0c08a7983727679262721c3
Sahal Mahfudh <=>
2022-07-01 20:05:25 +0700

    fix(jwt): remove middleware jwt on products and categories routes

--- internal/apps/products/route.go
+++ internal/apps/products/route.go
@@ -1,15 +1,13 @@
 package products
 
 import (
-	"belajar-go-echo/middleware"
-
 	"github.com/labstack/echo/v4"
 )
 
 func (h *handler) Route(g *echo.Group) {
 	g.GET("", h.GetAll)
 	g.GET("/:id", h.GetById)
-	g.POST("", h.Create, middleware.ValidateToken)
-	g.PUT("/:id", h.Update, middleware.ValidateToken)
-	g.DELETE("/:id", h.Delete, middleware.ValidateToken)
+	g.POST("", h.Create /*, middleware.ValidateToken*/)
+	g.PUT("/:id", h.Update /*, middleware.ValidateToken*/)
+	g.DELETE("/:id", h.Delete /*, middleware.ValidateToken*/)
 }
